bc2426c60dcae7c382436f36b00d5bff3325f05a
Will Comfort <comfortwb21@gmail.com>
2019-09-05 19:28:56 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,5 @@
+new_hash = {
+  :created => time.now 
+  :message => "Hello World"
+}
+new_hash
---
+++ learning-comments.rb
@@ -0,0 +1,119 @@
+#puts "Joy to the world"
+# puts "All the boys and girls"
+#puts "Joys to the fishes in the deep blue sea"
+#puts "Joy to you and me"
+
+#name = "Byron"
+# name = "Luca"
+
+#puts "We are sorry, but per health inspector's rules #{name} is not allowed in the store"
+
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+#rain = 0.7 
+#puts "Let's go outside!"
+#if rain > 0.5
+ # puts "Pack an umbrella!"
+#else
+ # puts "Enjoy the fine day!"
+#end
+#puts "Oh and always wear sunscreen!"
+
+#rain = 1000
+#if rain <= 0.25
+#  puts "pack a sun shelter!"
+#elsif (rain > 0.25 && rain < 0.75)
+#  puts "pack an umbrella!"
+#else
+#  puts "stay home and nap!"
+#end
+
+#puts "what year is it?"
+#this_year = 2019
+#puts "its 2019" if this_year = 2019
+
+#name = "The Mad Hatter"
+
+#if name == "Alice"
+ # puts "Hello Alice!"
+#elsif name == "The White Rabbit"
+ # puts "Dont be late White Rabbit"
+#elsif name == "The Mad Hatter"
+ # puts "Welcome to the tea party Mad Hatter"
+#elsif name == "Queen of Hearts"
+ # puts "Please dont chop off my head"
+#else
+ # puts "Who are you?"
+#end
+
+#case name
+
+#when "Alice"
+ # puts "hello Alice"
+#when "The White Rabbit"
+ # puts "Dont be late White Rabbit"
+#when "The Mad Hatter"
+ # puts "Welcome to the tea party Mad Hatter"
+#when "The Queen of Hearts"
+ # puts "Please dont chop off my head"
+#else 
+#  puts "Who are you?"
+#end 
+
+#count = 1
+#while count <= 18 do
+ # puts "I am #{count}, I love to count"
+#  count = count *= 2 
+#end 
+
+#magic_exit_number = 8 
+#while count < 10 do
+#  break if count == magic_exit_number
+#  puts "I am the #{count}, I love to count"
+# count = count + 1 
+#end 
+
+#n = 20
+#count = 0 
+#while count <= n do
+#  puts "I ran."
+#  count = count + 1 
+#end 
+
+#3.times do 
+#  puts "I ran"
+#end
+
+#loop do 
+#  break if count >= n 
+#  puts "I ran"
+#  count += 1
+#end
+
+#counter = 0 
+#ntil counter == 20
+#  puts "The current number is less than 20"
+#  counter += 1 
+#end 
+
+#def stylish_chef
+#  best_hairstyle = "Guy Fieri"
+#  return "Martha Stewart"
+#  "Guy Fieri"
+#end
+
+the_beetles = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
+
+the_beetles
+
+english_bands_by_city = { 
+  :liverpool => "The Beatles",
+  :manchester => "The Smiths",
+  :coventry => "Delia Derbyshire and the BBC Radiphonic band",
+  :london => "Ziggy Stardust and the Spiders from Mars"
+}
